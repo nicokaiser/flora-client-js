b7c174007ddfa86f145cd266cc6b40d5c82f29f6
Nico Kaiser <nico.kaiser@boerse-go.de>
2018-10-17 18:24:17 +0200

    Fix Node.js module format

--- webpack.config.js
+++ webpack.config.js
@@ -18,6 +18,7 @@ module.exports = [{
     devtool: 'source-map',
     output: {
         filename: 'index.node.js',
-        path: DIST_DIR
+        path: DIST_DIR,
+        libraryTarget: 'commonjs2'
     }
 }];
