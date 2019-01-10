fcdb879f15b92dd5f69d0888482763ca2f5c62e9
Martin Kleinhans <martin.kleinhans@boerse-go.de>
2019-01-10 16:00:59 +0100

    added adapter for ejecta-v8

---
+++ build/app.js
@@ -0,0 +1,14 @@
+'use strict';
+
+const Client = require('../src/Client');
+const Adapter = require('../src/Adapter/App');
+const timeout = require('./timeout');
+
+class FloraClient extends Client {
+    constructor(opts) {
+        opts.adapter = opts.adapter || new Adapter({ timeout: timeout(opts) });
+        super(opts);
+    }
+}
+
+module.exports = FloraClient;
