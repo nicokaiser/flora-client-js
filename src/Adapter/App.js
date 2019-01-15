65bfb744e31a2a72cc3e1e7d032ef3423b508600
Marco Baumgartl <marco.baumgartl@boerse-go.de>
2019-01-15 13:42:14 +0100

    Reformate code

--- src/Adapter/App.js
+++ src/Adapter/App.js
@@ -19,35 +19,36 @@ class App {
     }
 
     request(cfg) {
-        const headers = { ...cfg.headers };
+        const headers = {...cfg.headers};
         let body = null;
+
         if (cfg.httpMethod === 'POST') {
             headers['Content-Type'] = 'application/' + (cfg.jsonData ? 'json' : 'x-www-form-urlencoded');
             body = cfg.params ? querystringify(cfg.params) : cfg.jsonData;
         }
 
         return new Promise((resolve, reject) => {
-                const request = ajax.request(
-                        cfg.url,
-                        cfg.httpMethod,
-                        headers,
-                        body,
-                        this.timeout
-                    );
+            const request = ajax.request(
+                cfg.url,
+                cfg.httpMethod,
+                headers,
+                body,
+                this.timeout
+            );
 
-                request.always(function(response, statusCode) {
-                    if(typeof response !== 'object') {
-                        return reject(new Error('Invalid response received'));
-                    }
+            request.always(function (response, statusCode) {
+                if (typeof response !== 'object') {
+                    return reject(new Error('Invalid response received'));
+                }
 
-                    if (statusCode < 400) return resolve(response);
+                if (statusCode < 400) return resolve(response);
 
-                    const msg = response.error && response.error.message ? response.error.message : 'error';
-                    const err = new Error(msg);
-                    err.response = response;
-                    return reject(err);
-                });
+                const msg = response.error && response.error.message ? response.error.message : 'error';
+                const err = new Error(msg);
+                err.response = response;
+                return reject(err);
             });
+        });
     }
 }
 
