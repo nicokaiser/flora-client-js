cef9442c806fa86a33dacdabfb5f3bf5e4b6e401
Marco Baumgartl <marco.baumgartl@boerse-go.de>
2020-07-10 18:17:25 +0200

    Ignore eslint error for IE11 compatibility

--- src/util/valid-request-id.js
+++ src/util/valid-request-id.js
@@ -4,7 +4,9 @@ module.exports = (id) => {
     const type = typeof id;
 
     if (type === 'number') {
+        // eslint-disable-next-line no-restricted-globals
         if (isNaN(id)) return false;
+        // eslint-disable-next-line no-restricted-globals
         if (!isFinite(id)) return false;
     }
 
