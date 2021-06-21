91aa23df1e2c859a1d368789071628f32be84c51
Marco Baumgartl <marco.baumgartl@boerse-go.de>
2021-06-21 08:22:14 +0200

    Replace variable by destructuring declaration

--- build/timeout.js
+++ build/timeout.js
@@ -2,11 +2,8 @@
 
 const DEFAULT_TIMEOUT = 15000;
 
-module.exports = (opts) => {
-    const { timeout } = opts;
-
+module.exports = ({ timeout }) => {
     if (!timeout) return DEFAULT_TIMEOUT;
     if (Number.isNaN(Number(timeout))) return DEFAULT_TIMEOUT;
-
     return parseInt(timeout, 10);
 };
